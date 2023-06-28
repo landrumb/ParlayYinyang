86ed660e76a33ffa02a4b2dd4f40536b9e532e6c
Ben Landrum <8enlandrum@gmail.com>
2023-06-28 16:17:48 -0400

    added thread-local accumulator

---
+++ accumulator.h
@@ -0,0 +1,41 @@
+#include "parlay/parallel.h"
+#include "parlay/sequence.h"
+#include "parlay/primitives.h"
+
+#include <math.h>
+#include <stdlib.h>
+
+template <typename T>
+struct accumulator {
+
+    size_t width = 128 / sizeof(T);
+    parlay::sequence<T> counts = parlay::sequence<T>(parlay::num_workers() * width, (T)0);
+
+
+    accumulator() {}
+
+    void increment(){
+        counts[parlay::worker_id()*width]++;
+    }
+
+    void decrement(){
+        // probably not really valid on unsigned types but as long as the total is positive overflow should in theory make this work as intented anyways
+        counts[parlay::worker_id()*width]--;
+    }
+
+    void add(T val){
+        counts[parlay::worker_id()*width] += val;
+    }
+
+    void subtract(T val){
+        counts[parlay::worker_id()*width] -= val;
+    }
+
+    T total(){
+        return parlay::reduce(counts);
+    }
+
+    void reset(){
+        counts = parlay::sequence<T>(parlay::num_workers() * width, (T)0);
+    }
+};
